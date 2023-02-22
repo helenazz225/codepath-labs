aa720a3ddef4292497646af5ece0524a6c8bed6b
helena <helenazz225@gmail.com>
2023-02-21 21:53:50 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Calendar from './components/Calendar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 Days in Chicago</h1>
+      <h2>Welcome to Chicago, Helena! Check out this calendar to get to know the city and see all the sights during your stay.</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import Event from './Event';
+
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="time">8 am</td>
+                        <Event event='Starbucks ☕️' color ='green'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Yolk 🍳' color ='green'/>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">9 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Subway 🚊' color ='pink'/>
+                        <td></td>
+                        <td></td>
+                        <Event event='The Bean 🫘' color ='blue'/>
+                    </tr>
+                    <tr>
+                        <td className="time">10 am</td>
+                        <td></td>
+                        <Event event='River Cruise' color ='blue'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">11 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Deep Dish' color ='green'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">12 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Subway' color ='pink'/>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">1 pm</td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Art Institute' color ='blue'/>
+                        <td></td>
+                        <Event event='Girl & the Goat' color ='green'/>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">2 pm</td>
+                        <Event event='Cubs Game' color ='blue'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Subway' color ='pink'/>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">3 pm</td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Fancy Dinner' color ='blue' location='Maple & Ash'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">4 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Shopping' color ='blue'/>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Calendar;
